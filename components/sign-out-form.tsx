3e64edc3c6540ad229373e30453cae11ebf2fbcf
eshwarchedadeepu-cmyk <eshwar.chedadeepu@gmail.com>
2026-01-11 21:55:54 +0530

    Update sign-out-form.tsx

--- components/sign-out-form.tsx
+++ components/sign-out-form.tsx
@@ -12,25 +12,3 @@ export const SignOutForm = () => {
   );
 };
 
-
-export const SignOutForm = () => {
-  return (
-    <Form
-      action={async () => {
-        "use server";
-
-        await signOut({
-          redirectTo: "/",
-        });
-      }}
-      className="w-full"
-    >
-      <button
-        className="w-full px-1 py-0.5 text-left text-red-500"
-        type="submit"
-      >
-        Sign out
-      </button>
-    </Form>
-  );
-};
