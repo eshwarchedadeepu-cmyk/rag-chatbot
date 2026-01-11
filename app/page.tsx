77f2dd4939b460f9f3614750a1d00b2457263553
eshwarchedadeepu-cmyk <eshwar.chedadeepu@gmail.com>
2026-01-11 20:04:04 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,11 @@
+export const dynamic = "force-static";
+
 export default function Page() {
   return (
-    <div style={{ padding: 20 }}>
+    <div>
       <h1>RAG Chatbot is Live</h1>
-      <p>Deployment successful</p>
+      <p>App deployed successfully</p>
     </div>
   );
 }
+
